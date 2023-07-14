6ad6fa23f7993ac79ef329093edd0d4d90099edb
CS-Parra <carlos.parra.s@outlook.com>
2023-07-14 09:16:50 -0600

    second commit

---
+++ src/main/kotlin/com/cparra/models/Blog.kt
@@ -0,0 +1,6 @@
+package com.cparra.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Blog(val title: String, val author: String, val date: String, val content: String)
---
+++ src/main/kotlin/com/cparra/models/BlogsRoute.kt
@@ -0,0 +1,2 @@
+package com.cparra.models
+
